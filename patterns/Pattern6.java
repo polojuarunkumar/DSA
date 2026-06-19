43db809ed01de5f9cd93e036a5eb74c8e5e6a7bc
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-19 22:54:50 +0530

    Create Pattern6.java

---
+++ patterns/Pattern6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void pattern6(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output:
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
