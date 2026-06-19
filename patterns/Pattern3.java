f6fd7c0f0c26affedd69064638cbe0f7677599d7
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-19 22:39:01 +0530

    Add pattern printing method in Solution class
    Implement a method to print a right-angled triangle pattern of asterisks.

---
+++ patterns/Pattern3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void pattern2(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+//Output:
+
+*
+**
+***
+****
+*****
