eacd592a008afa04372e82109c065e8b30b6f43f
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-19 22:49:51 +0530

    Add pattern5 method to print star pattern
    Implement pattern5 method to print decreasing stars.

---
+++ patterns/Pattern5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void pattern5(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Output: 
+// ****
+// ***
+// **
+// *
