383e582e5acf96b23b1fa4a7363dcef09fcb579b
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-19 14:29:37 +0530

    Update CountNumberOfDigits.java

--- maths/CountNumberOfDigits.java
+++ maths/CountNumberOfDigits.java
@@ -2,9 +2,15 @@ package dsa.mathematics;
 
 public class CountNumberOfDigits {
 //    1. check if digits exists.
-//    2. Remove the last digit.
+//    2. Remove the last digit by and divide number by 10
 //    3. Increase value of count by 1.
 //    4. Repeat step 1 to step 3 till digits remains.
+
+// Example: Input: n=1234, Output=4
+// Explanation: 1234/10  = 123
+//               123/10  = 12
+//                12/10  = 1
+//                 1/10  = 0
 
 
     public static int countNumberOfDigits(int n){
