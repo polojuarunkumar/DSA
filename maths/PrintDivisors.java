b7cb52919cc62bba23d25aece3b3e2f1a744cf45
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-19 14:55:57 +0530

    Create PrintDivisors.java

---
+++ maths/PrintDivisors.java
@@ -0,0 +1,21 @@
+public class PrintDivisors{
+
+    static void printDivisors(int n){
+        int i=0;
+        for(i=1;i*i<=n;i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+        for(;i>=1;i--){
+            if(n%i==0 && i!=n/i){
+                System.out.println(n/i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printDivisors(40);
+    }
+
+}
