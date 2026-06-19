34dcfe947bf903ff11d46f793adb2ced6721e88c
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-19 22:55:48 +0530

    Add pattern10 method to print star patterns

---
+++ patterns/Pattern7.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void pattern10(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+         for(int i=1;i<n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+    }
+}
+
+//Output:
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
