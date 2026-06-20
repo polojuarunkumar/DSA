3a133d6108c8f0091354e305252fa49a339f5cea
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-20 10:35:59 +0530

    Create Pattern9.java

---
+++ patterns/Pattern9.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Main {
+    
+    public static void pattern7(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        pattern7(n);
+    }
+}
+
+// Output:
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
