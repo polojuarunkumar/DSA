a63fc6fcf11d908ed85a7ff23151daa35f7e8f85
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-20 10:41:04 +0530

    Create Pattern11.java

---
+++ patterns/Pattern11.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void pattern9(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            
+            
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=n-i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
+
+//Output:
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+// *********
+//  *******
+//   *****
+//    ***
+//     *
