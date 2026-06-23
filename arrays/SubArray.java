0601e952547a334066c5011796057aa4aa46b7c2
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-23 13:15:32 +0530

    Add SubArray class to print all subarrays

---
+++ arrays/SubArray.java
@@ -0,0 +1,18 @@
+public class SubArray {
+
+    static void subArray(int []ar){
+        for(int i=0;i<ar.length;i++){
+            for(int j=i;j<ar.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(ar[k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int ar[]={1,2,3,4,5};
+        subArray(ar);
+    }
+}
